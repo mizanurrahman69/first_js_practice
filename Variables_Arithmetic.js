3bbf5378f2f5f38b2e350ab19894247a9dba1961
mizanurrahman69 <abdulhamidemon0@gmail.com>
2024-05-08 04:41:18 +0600

    project init

---
+++ Variables_Arithmetic.js
@@ -0,0 +1,36 @@
+console.log("welcome to js");
+console.log("thanks");
+
+
+var first_Number;
+let second_number;
+const third_Number = 10;
+fourth_Number = 399;
+
+var first_Number = 200;
+let second_Number = 300;
+
+
+console.log(first_Number);
+console.log(second_Number);
+console.log(third_Number);
+console.log(fourth_Number);
+
+
+let a, b, sum_result, sub_result, mul_result;
+let div_result, mod_result;
+a = 10;
+b = 23;
+
+sum_result = a + b;
+sub_result = b - a;
+mul_result = a * b;
+div_result = a / b;
+mod_result = a % b;
+
+
+console.log("sum : " + sum_result);
+console.log('sub : ' + sub_result);
+console.log(`mul : ` + mul_result);
+console.log("div : " + div_result);
+console.log("mod : " + mod_result);
